e5e6936818dc6c16b44fad892612169c967e2557
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-04-01 14:51:12 -0400

    Add some negative test cases

--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -2,21 +2,90 @@ open OUnit2
 open Typecheck
 open Syntax
 
-(* TODO: Look for off-by-one errors here. *)
-let test_count_down _ =
+let count_down =
+  (*
+    let count_down : Pi n:Nat . Vec n =
+      fix count_down.\n.
+        match n with
+        | 0 -> nil
+        | m + 1 -> cons m m (count_down m)
+  *)
+  {
+    name = "count_down";
+    body =
+      Fix
+        ( "count_down",
+          Lam
+            ( "n",
+              NatMatch
+                ( Var "n",
+                  Nil,
+                  "m",
+                  Cons (sv "m", sv "m", Syn (App (Var "count_down", sv "m"))) )
+            ) );
+    typ = Pi ("n", Nat, Vec (Syn (Var "n")));
+  }
+
+let test_count_down _ = check_program [ count_down ]
+
+let test_count_down_wrong_base_case _ =
   let count_down =
-    Fix
-      ( "cnt",
-        Lam
-          ( "n",
-            NatMatch
-              ( Var "n",
-                Nil,
-                "m",
-                Cons (sv "m", sv "n", Syn (App (Var "cnt", sv "m"))) ) ) )
+    (*
+      let count_down : Pi n:Nat . Vec n =
+        fix count_down.\n.
+          match n with
+          | 0     -> cons 0 0 nil
+          | m + 1 -> cons m m (count_down m)
+    *)
+    {
+      name = "count_down";
+      body =
+        Fix
+          ( "count_down",
+            Lam
+              ( "n",
+                NatMatch
+                  ( Var "n",
+                    Cons (Num 0, Num 0, Nil),
+                    "m",
+                    Cons (sv "m", sv "m", Syn (App (Var "count_down", sv "m")))
+                  ) ) );
+      typ = Pi ("n", Nat, Vec (Syn (Var "n")));
+    }
   in
-  let typ = Pi ("n", Nat, Vec (Syn (Var "n"))) in
-  check Context.empty count_down typ
+  assert_raises
+    (Type_error "Term 'cons 0 0 nil' does not have the expected type 'Vec 0'.")
+    (fun _ -> check_program [ count_down ])
+
+let test_count_down_wrong_step_case _ =
+  let count_down =
+    (*
+      let count_down : Pi n:Nat . Vec n =
+        fix count_down.\n.
+          match n with
+          | 0     -> nil
+          | m + 1 -> cons n m (count_down n)
+    *)
+    {
+      name = "count_down";
+      body =
+        Fix
+          ( "count_down",
+            Lam
+              ( "n",
+                NatMatch
+                  ( Var "n",
+                    Cons (Num 0, Num 0, Nil),
+                    "m",
+                    Cons (sv "n", sv "m", Syn (App (Var "count_down", sv "n")))
+                  ) ) );
+      typ = Pi ("n", Nat, Vec (Syn (Var "n")));
+    }
+  in
+  assert_raises
+    (Type_error
+       "Term 'cons n m (count_down n)' does not have the expected type 'Vec n'.")
+    (fun _ -> check_program [ count_down ])
 
 let test_count_up _ =
   (*
@@ -177,6 +246,74 @@ let test_foldr _ =
   in
   check Context.empty foldr typ
 
+let concat =
+  {
+    name = "concat";
+    body =
+      Fix
+        ( "concat",
+          Lam
+            ( "n",
+              Lam
+                ( "m",
+                  Lam
+                    ( "v1",
+                      Lam
+                        ( "v2",
+                          VecMatch
+                            ( Var "v1",
+                              Syn (Var "v2"),
+                              "n'",
+                              "x",
+                              "xs",
+                              Cons
+                                ( Sum [ sv "n'"; sv "m" ],
+                                  sv "x",
+                                  Syn
+                                    (apps (Var "concat")
+                                       [ sv "n'"; sv "m"; sv "xs"; sv "v2" ]) )
+                            ) ) ) ) ) );
+    typ =
+      Pi
+        ( "n",
+          Nat,
+          Pi
+            ( "m",
+              Nat,
+              arrows
+                [ Vec (sv "n"); Vec (sv "m"); Vec (Sum [ sv "n"; sv "m" ]) ] )
+        );
+  }
+
+let test_concat _ =
+  let example_use_0 =
+    (* let ex0 : Vec 0 = concat 0 0 nil nil *)
+    {
+      name = "ex0";
+      body = Syn (apps (Var "concat") [ Num 0; Num 0; Nil; Nil ]);
+      typ = Vec (Num 0);
+    }
+  in
+  let n = { name = "n"; body = Fix ("n", sv "n"); typ = Nat } in
+  let m = { name = "m"; body = Fix ("n", sv "n"); typ = Nat } in
+  let example_use_1 =
+    (* let ex1 : Vec (n+m) = concat n m (count_down n) (count_down m) *)
+    {
+      name = "ex1";
+      body =
+        Syn
+          (apps (Var "concat")
+             [
+               sv "n";
+               sv "m";
+               Syn (App (Var "count_down", sv "n"));
+               Syn (App (Var "count_down", sv "m"));
+             ]);
+      typ = Vec (Sum [ sv "n"; sv "m" ]);
+    }
+  in
+  check_program [ count_down; concat; example_use_0; n; m; example_use_1 ]
+
 let zip_with =
   {
     name = "zip_with";
@@ -217,45 +354,165 @@ let zip_with =
             ] );
   }
 
-let test_zip_with _ = check_program [ zip_with ]
+let test_zip_with _ =
+  let plus = Lam ("a", Lam ("b", Sum [ sv "a"; sv "b" ])) in
+  let example_use_0 =
+    (* let ex0 : Vec 0 = zip_with 0 nil nil (+) *)
+    {
+      name = "ex0";
+      body = Syn (apps (Var "zip_with") [ Num 0; Nil; Nil; plus ]);
+      typ = Vec (Num 0);
+    }
+  in
+  let n = { name = "n"; body = Fix ("x", sv "x"); typ = Nat } in
+  let m = { name = "m"; body = Fix ("x", sv "x"); typ = Nat } in
+  let example_use_1 =
+    (* let ex1 : Vec n = zip_with n (count_down n) (count_down n) (+) *)
+    {
+      name = "ex1";
+      body =
+        Syn
+          (apps (Var "zip_with")
+             [
+               sv "n";
+               Syn (App (Var "count_down", sv "n"));
+               Syn (App (Var "count_down", sv "n"));
+               plus;
+             ]);
+      typ = Vec (sv "n");
+    }
+  in
+  let example_use_2 =
+    (*
+      let ex2 : Vec (n+m+1) =
+        zip_with
+          (1 + m + n)
+          (concat (n+1) m (count_down (n + 1)) (count_down m))
+          (concat (n+m) 1 (count_down (n + m)) (count_down 1))
+          (+)
+    *)
+    {
+      name = "ex2";
+      body =
+        Syn
+          (apps (Var "zip_with")
+             [
+               Sum [ Num 1; sv "m"; sv "n" ];
+               Syn
+                 (apps (Var "concat")
+                    [
+                      Sum [ sv "n"; Num 1 ];
+                      sv "m";
+                      Syn (App (Var "count_down", Sum [ sv "n"; Num 1 ]));
+                      Syn (App (Var "count_down", sv "m"));
+                    ]);
+               Syn
+                 (apps (Var "concat")
+                    [
+                      Sum [ sv "n"; sv "m" ];
+                      Num 1;
+                      Syn (App (Var "count_down", Sum [ sv "n"; sv "m" ]));
+                      Syn (App (Var "count_down", Num 1));
+                    ]);
+               plus;
+             ]);
+      typ = Vec (Sum [ sv "n"; sv "m"; Num 1 ]);
+    }
+  in
+  check_program
+    [ concat; zip_with; example_use_0; n; m; example_use_1; example_use_2 ]
 
-let test_concat _ =
-  let concat =
-    Fix
-      ( "concat",
+let test_zip_with_wrong_size_0_vs_1 _ =
+  let plus = Lam ("a", Lam ("b", Sum [ sv "a"; sv "b" ])) in
+  let v0 = { name = "v0"; body = Nil; typ = Vec (Num 0) } in
+  let v1 =
+    { name = "v1"; body = Cons (Num 0, Num 42, Nil); typ = Vec (Num 1) }
+  in
+  let zipped0 =
+    {
+      name = "zipped";
+      body = Syn (apps (Var "zip_with") [ Num 0; sv "v0"; sv "v1"; plus ]);
+      typ = Vec (Num 0);
+    }
+  in
+  let zipped1 =
+    {
+      name = "zipped";
+      body = Syn (apps (Var "zip_with") [ Num 1; sv "v0"; sv "v1"; plus ]);
+      typ = Vec (Num 1);
+    }
+  in
+  assert_raises
+    (Type_error "Term 'v1' does not have the expected type 'Vec 0'.") (fun _ ->
+      check_program [ zip_with; v0; v1; zipped0 ]);
+  assert_raises
+    (Type_error "Term 'v0' does not have the expected type 'Vec 1'.") (fun _ ->
+      check_program [ zip_with; v0; v1; zipped1 ])
+
+let test_zip_with_wrong_size_n_vs_m _ =
+  let plus = Lam ("a", Lam ("b", Sum [ sv "a"; sv "b" ])) in
+  let ex0 =
+    (*
+      let sum_any : Pi n:Nat . Pi m:Nat . Vec n -> Vec m -> Vec n =
+        \n.\m.\v0.\v1.
+          zip_with n v0 v1 (+)
+    *)
+    {
+      name = "sum_any";
+      body =
         Lam
           ( "n",
             Lam
               ( "m",
                 Lam
-                  ( "v1",
+                  ( "v0",
                     Lam
-                      ( "v2",
-                        VecMatch
-                          ( Var "v1",
-                            Syn (Var "v2"),
-                            "n'",
-                            "x",
-                            "xs",
-                            Cons
-                              ( Sum [ sv "n'"; sv "m" ],
-                                sv "x",
-                                Syn
-                                  (apps (Var "concat")
-                                     [ sv "n'"; sv "m"; sv "xs"; sv "v2" ]) ) )
-                      ) ) ) ) )
-  in
-  let typ =
-    Pi
-      ( "n",
-        Nat,
+                      ( "v1",
+                        Syn
+                          (apps (Var "zip_with")
+                             [ sv "n"; sv "v0"; sv "v1"; plus ]) ) ) ) );
+      typ =
         Pi
-          ( "m",
+          ( "n",
             Nat,
-            arrows [ Vec (sv "n"); Vec (sv "m"); Vec (Sum [ sv "n"; sv "m" ]) ]
-          ) )
+            Pi ("m", Nat, arrows [ Vec (sv "n"); Vec (sv "m"); Vec (sv "n") ])
+          );
+    }
   in
-  check Context.empty concat typ
+  let ex1 =
+    (*
+      let sum_any : Pi n:Nat . Pi m:Nat . Vec n -> Vec m -> Vec m =
+        \n.\m.\v0.\v1.
+          zip_with m v0 v1 (+)
+    *)
+    {
+      name = "sum_any";
+      body =
+        Lam
+          ( "n",
+            Lam
+              ( "m",
+                Lam
+                  ( "v0",
+                    Lam
+                      ( "v1",
+                        Syn
+                          (apps (Var "zip_with")
+                             [ sv "m"; sv "v0"; sv "v1"; plus ]) ) ) ) );
+      typ =
+        Pi
+          ( "n",
+            Nat,
+            Pi ("m", Nat, arrows [ Vec (sv "n"); Vec (sv "m"); Vec (sv "m") ])
+          );
+    }
+  in
+  assert_raises
+    (Type_error "Term 'v1' does not have the expected type 'Vec n'.") (fun _ ->
+      check_program [ zip_with; ex0 ]);
+  assert_raises
+    (Type_error "Term 'v0' does not have the expected type 'Vec m'.") (fun _ ->
+      check_program [ zip_with; ex1 ])
 
 let take =
   (*
@@ -487,6 +744,7 @@ let test_dot _ =
   in
   check_program [ foldl; vec_sum; zip_with; times; dot ]
 
+(* Here's how you can specify that a vector must have even length. *)
 let test_first_half _ =
   let first_half =
     (*
@@ -505,19 +763,23 @@ let test_first_half _ =
 let tests =
   "typecheck"
   >::: [
-         "count_down" >:: test_count_down;
-         "count_up" >:: test_count_up;
-         "map" >:: test_map;
-         "foldl" >:: test_foldl;
-         "foldr" >:: test_foldr;
-         "zip_with" >:: test_zip_with;
-         "concat" >:: test_concat;
-         "take" >:: test_take;
-         "take_wrong_base_case" >:: test_take_wrong_base_case;
-         "drop" >:: test_drop;
-         "drop_wrong_base_case" >:: test_drop_wrong_base_case;
-         "dot" >:: test_dot;
-         "first_half" >:: test_first_half;
+         "count_down:ok" >:: test_count_down;
+         "count_down:wrong base case" >:: test_count_down_wrong_base_case;
+         "count_down:wrong step case" >:: test_count_down_wrong_step_case;
+         "count_up:ok" >:: test_count_up;
+         "map:ok" >:: test_map;
+         "foldl:ok" >:: test_foldl;
+         "foldr:ok" >:: test_foldr;
+         "zip_with:ok" >:: test_zip_with;
+         "zip_with:0 with 1" >:: test_zip_with_wrong_size_0_vs_1;
+         "zip_with:n with m" >:: test_zip_with_wrong_size_n_vs_m;
+         "concat:ok" >:: test_concat;
+         "take:ok" >:: test_take;
+         "take:wrong base case" >:: test_take_wrong_base_case;
+         "drop:ok" >:: test_drop;
+         "drop:wrong base case" >:: test_drop_wrong_base_case;
+         "dot:ok" >:: test_dot;
+         "first_half:ok" >:: test_first_half;
          ( "synth_success_no_dependent_types" >:: fun _ ->
            let ctxt =
              Context.of_seq
